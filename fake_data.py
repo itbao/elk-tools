9edb837548d32cd6e5ed70ac0a6a639e3e4e9abb
xuchangbao <xuchangbaoxcb@hfbank.com.cn>
2017-08-31 20:59:29 +0800

    bugfix fake_date.py

--- fake_data.py
+++ fake_data.py
@@ -9,40 +9,35 @@ from elasticsearch import Elasticsearch
 import argparse
 import json
 import time
-from datetime import datetime,timedelta
+from datetime import datetime, timedelta
 
 
-
-def es_fill_data(es,num,index,_type,keylist):
+def es_fill_data(es, num, index, _type, keylist):
 
     fake = Factory.create()
     number = 0
 
     for x in range(int(num)):
 
-        genName = fake.name()
-        genJob = fake.job()
-        genEmail = fake.email()
-
-        #global number
         number -= 1
 
-        timestamp = datetime.utcnow() + timedelta(minutes = number)
+        timestamp = datetime.utcnow() + timedelta(minutes=number)
+        body = {}
+        for key in keylist:
+            methd = "fake.%s()" % key
+            value = eval(methd)
+            body.update({key: value})
+
+        body.update({'timestamp': timestamp})
 
         go = es.index(
             index=index,
             doc_type=_type,
-            #id=str(number),
-            body={
-                "name": genName,
-                "job": genJob,
-                "email": genEmail,
-                'timestamp': timestamp
-            }
+            body=body
         )
 
         print json.dumps(go, indent=4)
-        #time.sleep(0.01)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -53,7 +48,9 @@ if __name__ == '__main__':
     parser.add_argument('--index', '-i')
     parser.add_argument('--type', '-t')
     parser.add_argument('--number', '-n', default=1)
-    parser.add_argument('--data', '-d', nargs='*')
+    parser.add_argument('--data', '-d', nargs='*',
+                        choices=("ipv4", "name", "email")
+                        )
     parser.add_argument('--server', '-s', default='127.0.0.1')
     parser.add_argument('--port', '-p', default='9200')
     parser.add_argument('--verbose', '-v', action='count')
@@ -62,14 +59,13 @@ if __name__ == '__main__':
     print args
 
     es_host = "http://{server}:{port}".format(server=args['server'],
-            port=args['port'])
+                                              port=args['port'])
     es = Elasticsearch(es_host)
 
     es_fill_data(
-            es,
-            num=args['number'],
-            index=args['index'],
-            _type=args['type'],
-            keylist=args['data']
-            )
-
+        es,
+        num=args['number'],
+        index=args['index'],
+        _type=args['type'],
+        keylist=args['data']
+    )
